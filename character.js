6841ac7a56a25bceda181486ebdfd5f8d9c29dbf
Jon Petto <jonpetto@gmail.com>
2018-10-26 18:55:12 -0500

    First touches of localStorage to persist game state. 🧙‍♂️

---
+++ character.js
@@ -0,0 +1,19 @@
+const character = {
+    "name": "Caturra Maitu",
+    "class": "Ranger",
+    "race": "Wood Elf",
+    "level": 6,
+    "experience": 16985,
+    "hp": 35,
+    "spell_slots": {
+        "1": 4,
+        "2": 2
+    },
+    "spell_save_dc": 14,
+    "spell_attack_bonus": 6,
+    "ac": 17,
+    "initiative": 4,
+    "proficiency": 3
+};
+
+export default character;
--- index.js
+++ index.js
@@ -1,22 +1,157 @@
+import character from './character.js';
+
 (function() {
     'use strict';
 
+    console.log(character);
+
     const SESSIONFORM = document.getElementById('session-form');
+
+    const SESSIONSTART = document.getElementById('session-start');
 
     const HP = document.getElementById('hp');
     const HPUP = document.getElementById('hp-up');
     const HPDN = document.getElementById('hp-dn');
     // so we can know the max value
-    const HPMAX = parseInt(HP.value, 10);
+    const HPMAX = character.hp;
 
     const HITDICE = document.getElementById('hit-dice');
     const HITDICEUP = document.getElementById('hit-dice-up');
     const HITDICEDN = document.getElementById('hit-dice-dn');
     // so we can know the max value
-    const HITDICEMAX = parseInt(HITDICE.value, 10);
+    const HITDICEMAX = character.level;
 
     const DEATHSAVERADIOS = document.querySelectorAll('.death-save-radio');
+    const DEATHSAVESTABLE = document.getElementById('death-saves-table');
     const DEATHSAVESRESET = document.getElementById('death-saves-reset');
+
+    const AC = document.getElementById('ac');
+    const PROFICIENCY = document.getElementById('proficiency');
+    const INITIATIVE = document.getElementById('initiative');
+    const SPELLSAVEDC = document.getElementById('spell-save-dc');
+    const SPELLATTACKBONUS = document.getElementById('spell-attack-bonus');
+
+    const GAMETEMPLATE = {
+        "id": null,
+        "date": new Date(),
+        "goal": null,
+        "hp_start": character.hp,
+        "hp_end": character.hp,
+        "hitdice_start": character.level,
+        "hitdice_end": character.level,
+        "death_saves": [null, null, null],
+        "xp_earned": null,
+        "spell_slots": [
+            {
+                "1": {
+                    "begin": 4,
+                    "end": 4
+                },
+                "2": {
+                    "begin": 2,
+                    "end": 2
+                }
+            }
+        ],
+        "notes": "",
+        "money": {
+            "gold": 0,
+            "silver": 0,
+            "bronze": 0
+        },
+        "items": [],
+        "players": []
+    }
+
+    let thisGame;
+
+    // build spell slots check boxes
+    function initSpells(character) {
+
+    }
+
+    function initGame(game) {
+        console.log('initializing game:');
+        console.log(game);
+
+        // set hit points & hit dice
+        HP.value = game.hp_end;
+        HITDICE.value = game.hitdice_end;
+
+        // set death saves
+        for (let i = 1; i < 4; i++) {
+            // if a save is logged, render its punk ass
+            if (game.death_saves[i - 1] !== null) {
+                const THESAVE = document.getElementById(`death-save-${i}-${game.death_saves[i - 1] === true ? 'success' : 'failure'}`);
+                THESAVE.checked = true;
+            }
+        }
+
+        // set spell slots used
+
+        // set money
+
+        // set notes
+
+        // copy template to a new object & save game to localStorage
+        thisGame = Object.assign({}, game);
+
+        localStorage.setItem('game', JSON.stringify(thisGame));
+    }
+
+    // check localStorage for a game that was started today
+    // if it exists, use HP/Hit dice from there
+    // otherwise, use defaults from character
+    function init(character) {
+        var game = localStorage.getItem('game');
+
+        // if we have a game, check the date
+        if (game) {
+            game = JSON.parse(game);
+
+            // make sure game is from today
+            var saveDate = new Date(game.date);
+            var today = new Date();
+
+            if (saveDate.setHours(0, 0, 0, 0) === today.setHours(0, 0, 0, 0)) {
+                console.log('loading saved game!');
+                thisGame = game;
+            } else {
+                console.log('beginning new game');
+                thisGame = GAMETEMPLATE;
+            }
+        } else {
+            thisGame = GAMETEMPLATE;
+        }
+
+        initGame(thisGame);
+
+        // this stuff isn't saved per game - kind of forward coded for now in
+        // anticipation of fully loading a character from JS
+        AC.textContent = character.ac;
+        PROFICIENCY.textContent = `+${character.proficiency}`;
+        INITIATIVE.textContent = `+${character.initiative}`;
+        SPELLSAVEDC.textContent = character.spell_save_dc;
+        SPELLATTACKBONUS.textContent = `+${character.spell_attack_bonus}`;
+
+        initSpells(character);
+    }
+
+    init(character);
+
+    SESSIONSTART.addEventListener('click', () => {
+
+    });
+
+    function updateGame(key, val) {
+        if (thisGame.hasOwnProperty(key)) {
+            thisGame[key] = val;
+            console.log(thisGame);
+            localStorage.setItem('game', JSON.stringify(thisGame));
+        } else {
+            console.log(`No property named ${key}!`);
+        }
+    }
 
     function increment(current, max) {
         if (current + 1 <= max) {
@@ -43,6 +178,8 @@
         } else if (NEWHP > 0) {
             HP.classList.remove('dead');
         }
+
+        updateGame('hp_end', NEWHP);
     });
 
     HPDN.addEventListener('click', () => {
@@ -56,17 +193,42 @@
                 HP.classList.add('dead');
             }
         }
+
+        updateGame('hp_end', NEWHP);
     });
 
     HITDICEUP.addEventListener('click', () => {
         const NEWHITDICE = increment(parseInt(HITDICE.value, 10), HITDICEMAX);
-
         HITDICE.value = NEWHITDICE;
+
+        updateGame('hitdice_end', NEWHITDICE);
     });
 
     HITDICEDN.addEventListener('click', () => {
         const NEWHITDICE = decrement(parseInt(HITDICE.value, 10));
         HITDICE.value = NEWHITDICE;
+
+        updateGame('hitdice_end', NEWHITDICE);
+    });
+
+    DEATHSAVESTABLE.addEventListener('click', e => {
+        if (e.target.classList.contains('death-save-radio')) {
+            const SAVENUMBER = parseInt(e.target.dataset.deathSaveNumber, 10);
+            const SUCCESS = (e.target.id.indexOf('success') >= 0) ? true : false;
+
+            let saves = thisGame.death_saves;
+
+            // make sure each save has some value - even if it's null
+            for (let i = 1; i < 4; i++) {
+                if (SAVENUMBER === i) {
+                    saves[i - 1] = SUCCESS;
+                } else {
+                    saves[i - 1] = (saves[i - 1] !== null) ? saves[i - 1] : null;
+                }
+            }
+
+            updateGame('death_saves', saves);
+        }
     });
 
     DEATHSAVESRESET.addEventListener('click', () => {
@@ -88,10 +250,10 @@
         });
     });
 
-    TOGGLER_CONTENTS.forEach( element => {
+    TOGGLER_CONTENTS.forEach(element => {
         element.addEventListener('click', () => {
             element.classList.add('hidden');
         })
-    })
+    });
 
 })();
