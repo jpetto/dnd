308b14343cd1e811c1c8929408ecfeda87d45fd8
Jonathan Petto <jpetto@users.noreply.github.com>
2019-04-22 10:00:31 -0500

    Add Blink spell

--- assets/js/character.js
+++ assets/js/character.js
@@ -303,6 +303,17 @@ const character = {
                     "description": "This spell lets you convince a beast that you mean it no harm. Choose a beast that you can see within range. It must see and hear you. If the beast's Intelligence is 4 or higher, the spell fails. Otherwise, the beast must succeed on a Wisdom saving throw or be charmed by you for the spell's duration. If you or one of your companions harms the target, the spells ends.\r\n** SNAKES ONLY **"
                 },
                 {
+                    "name": "Blink",
+                    "level": 3,
+                    "school": "Transmutation",
+                    "casting_time": "1 action",
+                    "range": "self",
+                    "components": "V S",
+                    "duration": "1 minute",
+                    "classes": "Sorceror, Wizard",
+                    "description": "Roll a d20 at the end of each of your turns for the duration of the spell. On a roll of 11 or higher, you vanish from your current plane of existence and appear in the Ethereal Plane (the spell fails and the casting is wasted if you were already on that plane). At the start of your next turn, and when the spell ends if you are on the Ethereal Plane, you return to an unoccupied space of your choice that you can see within 10 feet of the space you vanished from. If no unoccupied space is available within that range, you appear in the nearest unoccupied space (chosen at random if more than one space is equally near). You can dismiss this spell as an action.\r\nWhile on the Ethereal Plane, you can see and hear the plane you originated from, which is cast in shades of gray, and you can’t see anything there more than 60 feet away. You can only affect and be affected by other creatures on the Ethereal Plane. Creatures that aren’t there can’t perceive you or interact with you, unless they have the ability to do so.\r\nEach time the wearer enters the Ethereal, they must roll a 1d20; on a 1-2, the armor attacts the attention of a creature on the Ethereal Plane."
+                },
+                {
                     "name": "Flame Shield",
                     "level": 4,
                     "school": "Evocatioin",
