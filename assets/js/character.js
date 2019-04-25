6352bbeead1182717017e1199449ade2a3629fe0
Jonathan Petto <jpetto@users.noreply.github.com>
2019-04-25 12:04:43 -0500

    Update XP from additional adventure log

--- assets/js/character.js
+++ assets/js/character.js
@@ -11,7 +11,7 @@ const character = {
         }
     ],
     "race": "Wood Elf",
-    "experience": 79150,
+    "experience": 79575,
     "experience_next": 85000,
     "hp": 52,
     "spell_modifier": "wis",
