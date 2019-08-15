37ec09de6d71d8bdca9c3eaaefeb4bd04bbfd7dd
Jonathan Petto <jpetto@users.noreply.github.com>
2019-08-15 14:21:32 -0500

    add banishment spell (horizon walker lvl 13)

--- assets/js/character.js
+++ assets/js/character.js
@@ -195,6 +195,17 @@ const character = {
                     "description": "A wall of strong wind rises from the ground at a point you choose within range. You can make the wall up to 50 feet long, 15 feet high, and 1 foot thick. You can shape the wall in any way you choose so long as it makes one continuous path along the ground. The wall lasts for the duration.\r\nWhen the wall appears, each creature within its area must make a Strength saving throw. A creature takes 3d8 bludgeoning damage on a failed save, or half as much damage on a successful one.\r\nThe strong wind keeps fog, smoke, and other gases at bay. Small or smaller flying creatures or objects can’t pass through the wall. Loose, lightweight materials brought into the wall fly upward. Arrows, bolts, and other ordinary projectiles launched at targets behind the wall are deflected upward and automatically miss. (Boulders hurled by giants or siege engines, and similar projectiles, are unaffected.) Creatures in gaseous form can’t pass through it."
                 },
                 {
+                    "name": "Banishment",
+                    "level": 4,
+                    "school": "Abjuration",
+                    "casting_time": "1 action",
+                    "range": "60 feet",
+                    "components": "V S M",
+                    "duration": "Concentration, up to 1 minute",
+                    "classes": "Paladin, Cleric, Sorcerer, Warlock, Wizard",
+                    "description": "You attempt to send one creature that you can see within range to another plane of existence. The target must succeed on a Charisma saving throw or be banished.\r\nIf the target is native to the plane of existence you're on, you banish the target to a harmless demiplane. While there, the target is incapacitated. The target remains there until the spell ends, at which point the target reappears in the space it left or in the nearest unoccupied space if that space is occupied.\r\nIf the target is native to a different plane of existence than the one you're on, the target is banished with a faint popping noise, returning to its home plane.\r\nIf the spell ends before 1 minute has passed, the target reappears in the space it left or in the nearest unoccupied space if that space is occupied. Otherwise, the target doesn't return.\r\nWhen you cast this spell using a spell slot of 5th level or higher, you can target one additional creature for each slot level above 4th."
+                },
+                {
                     "name": "Etherealness",
                     "level": 7,
                     "school": "Transmutation",
