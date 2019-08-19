887a8a41d84940fd7ff6cc55431cbfa5d1545917
Jonathan Petto <jpetto@users.noreply.github.com>
2019-08-19 16:00:10 -0500

    add guardian of nature spell

--- assets/js/character.js
+++ assets/js/character.js
@@ -206,6 +206,17 @@ const character = {
                     "description": "You attempt to send one creature that you can see within range to another plane of existence. The target must succeed on a Charisma saving throw or be banished.\r\nIf the target is native to the plane of existence you're on, you banish the target to a harmless demiplane. While there, the target is incapacitated. The target remains there until the spell ends, at which point the target reappears in the space it left or in the nearest unoccupied space if that space is occupied.\r\nIf the target is native to a different plane of existence than the one you're on, the target is banished with a faint popping noise, returning to its home plane.\r\nIf the spell ends before 1 minute has passed, the target reappears in the space it left or in the nearest unoccupied space if that space is occupied. Otherwise, the target doesn't return.\r\nWhen you cast this spell using a spell slot of 5th level or higher, you can target one additional creature for each slot level above 4th."
                 },
                 {
+                    "name": "Guardian of Nature",
+                    "level": 4,
+                    "school": "Transmutation",
+                    "casting_time": "1 bonus action",
+                    "range": "self",
+                    "components": "V",
+                    "duration": "Concentration, up to 1 minute",
+                    "classes": "Druid, Ranger",
+                    "description": "A nature spirit answers your call and transforms you into a powerful guardian. The transformation lasts until the spell ends. You choose one of the following forms to assume: Primal Beast or Great Tree.\r\n\r\nPrimalBeast. Bestial fur coversyour body, your facial features become feral, and you gain the following benefits:\r\n- Your walking speed increases by 10 feet.\r\n- You gain dark vision with a range of 120 feet.\r\n- You make Strength—based attack rolls with advantage.\r\n- Your melee weapon attacks deal an extra 1d6 force damage on a hit.\r\n\r\nGreat Tree. Your skin appears barky, leaves sprout from your hair, and you gain the following benefits:\r\n- You gain 10 temporary hit points.\r\n- You make Constitution saving throws with advantage.\r\n- You make Dexterity- and Wisdom-based attack rolls with advantage.\r\n- While you are on the ground, the ground within 15 feet of you is difficult terrain for your enemies."
+                },
+                {
                     "name": "Etherealness",
                     "level": 7,
                     "school": "Transmutation",
